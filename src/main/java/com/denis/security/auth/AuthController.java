1fbdd5ca59f26ee7e183300fc19db61525ba2d82
Denis Duci <denis.duci@check24.de>
2024-03-28 14:13:15 +0100

    remove white line

--- src/main/java/com/denis/security/auth/AuthController.java
+++ src/main/java/com/denis/security/auth/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-
     private final AuthService authService;
 
     @Autowired
